d0c222a9e977a69dc3c166ab4c3b319c9d032dd2
gucchi0421 <gucchi.1999.1121.0421@gmail.com>
2026-05-28 14:52:17 +0900

    fix(article_extract_html): re-escape HTML inside <pre> blocks
    HTMLParserのhandle_dataは&lt;等をデコードした生テキストで渡すため、
    <pre><code>内のHTMLエスケープが解除されてWPに生タグが投稿されていた問題を修正。
    <pre>内のテキストはescapeで再エスケープして出力する。

--- scripts/article_extract_html.py
+++ scripts/article_extract_html.py
@@ -2,12 +2,14 @@
 """HTMLファイルから <article> タグの中身を抽出して stdout に出力する"""
 import sys
 from html.parser import HTMLParser
+from html import escape
 
 
 class ArticleExtractor(HTMLParser):
     def __init__(self):
         super().__init__()
         self.in_article = False
+        self.in_pre = False
         self.depth = 0
         self.content = []
 
@@ -20,6 +22,8 @@ class ArticleExtractor(HTMLParser):
             self.depth += 1
             attr_str = ''.join(f' {k}="{v}"' for k, v in attrs if v)
             self.content.append(f'<{tag}{attr_str}>')
+            if tag == 'pre':
+                self.in_pre = True
 
     def handle_endtag(self, tag):
         if self.in_article:
@@ -28,11 +32,17 @@ class ArticleExtractor(HTMLParser):
                 if self.depth == 0:
                     self.in_article = False
                 return
+            if tag == 'pre':
+                self.in_pre = False
             self.content.append(f'</{tag}>')
 
     def handle_data(self, data):
         if self.in_article:
-            self.content.append(data)
+            # <pre>内はHTMLエスケープして再エスケープ済み状態を保つ
+            if self.in_pre:
+                self.content.append(escape(data, quote=False))
+            else:
+                self.content.append(data)
 
 
 if __name__ == '__main__':
